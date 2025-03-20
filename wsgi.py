0a83d9ab9c1381a5fddac088838ebc987a30560a
oppie <lunasmarkjhon@gmail.com>
2025-03-20 09:44:52 +0800

    Fix wsgi.py path issue

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,10 @@
 import sys
+import os
+
 # Add your project directory to the Python path
-path = '/home/your-username/your-project-folder'
+path = os.path.dirname(os.path.abspath(__file__))  # Get the absolute path of the current file
 if path not in sys.path:
     sys.path.append(path)
 
+# Import your Flask app
 from app import app as application  # Replace 'app' with the name of your Flask app instance
